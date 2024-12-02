09f234c2f0a1d6f53cbd8a1418f8cf6ef3e72dbb
Pure1144 <purevdorj@enlight.mn>
2024-12-02 19:37:18 +0800

    first

---
+++ index.js
@@ -0,0 +1,128 @@
+console.log("Hello World")
+
+// let, const, var ==> variable  ==> hairtsag ==> hadgaly
+
+let age = 15; //ar taliin too  value uurchilj bolno//
+
+console.log(age);   //age- iig hevlej harj bna//
+
+age = 40;             //age- iig uurchilj bna.//
+
+console.log(age);             // uurchlultiig hevlej harj bna
+
+age = true;
+
+let myName = "Bat";
+const mySonName = "elbeg";
+
+console.log(myName);
+
+myName = "Elbeg";
+
+
+console.log(myName);
+const address = undefined;
+const streetname = null;
+
+var year = 2000;
+console.log(year);  // 4. Утга оноогоогүй хувьсагч зарлах
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+console.log(a + b + c);   // 8. Гурвалжингийн периметрийг олох
+
+let r = 10;
+const p = 3.14;
+
+
+console.log(r * r * p);   // 9. Тойргийн талбайг олох
+
+
+
+let y = 10;
+
+let x = 5;
+
+console.log((y*2 - y*1)/(x*2 - x*1))    // 10. m = y2-y1/x2-x1 хариуг олох
+
+
+let v = 11.15;
+
+console.log(r/3*1000000);                // 11. 15 МB дата 3 секундэд манай интернэт онлайнаар татаж чаддаг бол түүнийг BIT Rate-ийг олно уу.
+
+side1 = 10;
+side2 = 20;
+side3 = 10;
+
+console.log(Math.sqrt(30*(30-side1)*(30-side2)*(30-side3)));   // 12. side1, side2, side3 гэсэн хувьсагчдад гурвалжны гурван талыг онооно. Ийнхүү гурван тал өгөдсөн байхад үед гурвалжны талбайг ол.
+
+celsius = 10;            
+
+
+console.log(celsius *9/5+32);    // 13. Өгсөн тоог цельсээс фаренгейт рүү хөрвүүл.
+
+teamA1 = 96;
+teamA2= 108;
+teamA3= 89;  
+teamB1 = 88;
+teamB2= 91;
+teamB3= 110;                             // 14. Хоёр багийн дундаж оноог олно уу. ( A team - 96, 108, 89; B team - 88, 91, 110 )
+
+console.log((teamA1+teamA2+teamA3)/3);    
+console.log((teamB1+teamB2+teamB3)/3);   
+console.log(((teamA1+teamA2+teamA3)+(teamB1+teamB2+teamB3))/3);   
+
+
+
+
+
+
+Huvis1 = 96;
+Huvis2 = 108;
+console.log("hoyor toonii niilber="+(Huvis1+Huvis2));            // 15. 2 хувьсагч зарлан 2 тоон утга өг. Тухайн 2 хувьсагчдаар Math-ийн 5н үйлдэлүүдийг (+-*/%) гүцэтгээд үр дүнг нь дараах байдлаар харуул:
+console.log("hoyor toonii yalgavar="+(Huvis1-Huvis2)); 
+console.log("hoyor toonii urjver="+(Huvis1*Huvis2)); 
+console.log("hoyor toonii noogdvor="+(Huvis1/Huvis2)); 
+
+
+//bodlogo-16
+Inch = 16;
+console.log("Inch to sm="+(Inch *2.54));     // 16. cm -г inch рүү хөрвүүлэх. inch -г cm рүү хөрвүүлэх
+
+sm = 16;
+console.log("Inch to sm="+(sm/ 2.54)); 
+
+
+//bodlogo-17
+
+
+
+const pi = 3.14;
+const radius=prompt("oruulna uu")
+
+console.log("ToirgiinTalbaiOloh="+(radius * radius* pi));  // 17. Гараас дугуйн радиус буюу r орж ирэхэд тухайн дугуйн эзэлхүүнийг ол. Гараас дугуйн радиус буюу r, орж ирэхэд тухайн дугуйн хүрээний уртыг ол.
+console.log("ToirgiinTalbaiOloh="+(2 * radius* pi)); 
+
+//bodlogo-18
+
+
+const gurvanoront=prompt("oruulna uu")
+let o =gurvanoront/100
+ console.log(("3nOrniiEhniiTsifr="+Math.floor(o)));    // 18. 3 оронтоо тооны хамгийн эхний цифрийг олох илэрхийлэл бич
+
+
+          
+
+
+
+
+
+//// 1. "Comments can make code readable" - нэг мөртэй коммент болгох
+// 2. Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+// 3. Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined, null гэх мэт
+// 4. Утга оноогоогүй хувьсагч зарлах
+// 5. Утга оноосон хувьсагч зарлах
+// 6. Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+// 7. Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах//
